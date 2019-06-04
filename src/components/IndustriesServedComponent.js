996f5c2d81f311be09e39ebd2817553ac21c3980
IT dept Valor Homes <itdept@valorhomes100llc.com>
2019-06-04 10:39:50 -0400

    industries 2

--- src/components/IndustriesServedComponent.js
+++ src/components/IndustriesServedComponent.js
@@ -98,7 +98,7 @@ function IndustriesServed () {
 
               </div>
 
-              <div className="col-12 col-md-4 mb-5">
+              <div className="col-12 col-md-4">
                 <h5>Leisure and Service </h5>
                 <ul className="mb-5">
                   <li>Amusement and recreations</li>
@@ -134,6 +134,355 @@ function IndustriesServed () {
                 </ul>
               </div>
 
+            </div>
+
+            <div className="row mt-4 ml-4 mr-4">
+
+              <div className="col-12 col-md-4">
+                <h5>Real Estate and Construction</h5>
+                <ul>
+                  <li>New construction</li>
+                  <li>Apartments</li>
+                  <li>Commercial</li>
+                  <li>Retail</li>
+                  <li>Residential</li>
+                  <li>Rehab</li>
+                  <li>Warehouses</li>
+                  <li>Shopping centers</li>
+                  <li>Developers and contractors</li>
+                </ul>
+              </div>
+
+              <div className="col-12 col-md-4">
+                <h5>Agri-Business	</h5>
+                <ul className="mb-5">
+                  <li>Crop farming</li>
+                  <li>Livestock farming</li>
+                  <li>Livestock processing  </li>
+                  <li>Genetic breeding</li>
+                  <li>Horticulture Supplies</li>
+                </ul>
+              </div>
+
+              <div className="col-12 col-md-4 mb-5">
+                <h5>Transportation</h5>
+                <ul className="mb-5">
+                  <li>Airlines and Air Cargo</li>
+                  <li>Freight Forwarders</li>
+                  <li>Railroad</li>
+                  <li>Shipping</li>
+                  <li>Trucking</li>
+                </ul>
+              </div>
+
+            </div>
+
+            <div className="row mt-4 ml-4 mr-4 mb-3">
+              <div className="col-12 mb-3">
+                <h2 className="mb-3">Affiliations</h2>
+                <h5>Equity Sponsors</h5>
+              </div>
+              <div className="col-12 col-md-4">
+                <ul>
+                  <li>American Capital Strategies</li>
+                  <li>BMO Nesbill Burns Equity</li>
+                  <li>Brannin Investments</li>
+                  <li>C-3 Holdings</li>
+                  <li>Cardinal Growth</li>
+                  <li>Castle Harlan</li>
+                  <li>Churchill Capital</li>
+                  <li>Code Hennessy & Simmons</li>
+                  <li>Deere Park Capital</li>
+                  <li>Delaware Street Partners</li>
+                  <li>FCP Investors, Inc.</li>
+                  <li>Fidelity Capital</li>
+                  <li>Full Circle Investments</li>
+                  <li>George K. Baum & Company</li>
+                  <li>Heath Investment Capital</li>
+                  <li>HIG Capital</li>
+                  <li>High Point Capital LLC</li>
+                  <li>Huron Capital</li>
+                  <li>Jacobson Partners</li>
+                  <li>John Hancock</li>
+                  <li>Kansas City Venture Capital</li>
+                </ul>
+              </div>
+
+              <div className="col-12 col-md-4">
+                <ul>
+                  <li>Lurie Investments</li>
+                  <li>Main Street Capital Holdings</li>
+                  <li>Mason Wells</li>
+                  <li>Mesirow Financial</li>
+                  <li>NPISCO</li>
+                  <li>Odyssey Partners</li>
+                  <li>PPM America/Equitable</li>
+                  <li>Prudential</li>
+                  <li>RDK Capital</li>
+                  <li>Recovery Equity Investors</li>
+                  <li>RFE Investment Partners</li>
+                  <li>Rocky Mountain Capital Partners</li>
+                  <li>Seidler & Associates</li>
+                  <li>St. Paul Venture Fund</li>
+                  <li>State of Wisconsin Investment Board</li>
+                  <li>Teachers Insurance & Annuity Assoc.</li>
+                  <li>Texas Growth Fund</li>
+                  <li>Three Cities Research</li>
+                  <li>TBM Holdings</li>
+                  <li>Westgate Partners</li>
+                </ul>
+              </div>
+
+
+            </div>
+
+            <div className="row mt-4 ml-4 mr-4 mb-3">
+              <div className="col-12 mb-3">
+                <h5>Attorneys</h5>
+              </div>
+              <div className="col-12 col-md-4">
+                <ul>
+                  <li>Alston & Bird LLP</li>
+                  <li>Altheimer & Gray</li>
+                  <li>Armstrong Teasdale LLP</li>
+                  <li>Arnall, Golden & Gregory LLP</li>
+                  <li>Baker & Daniels</li>
+                  <li>Barnes & Thornberg</li>
+                  <li>Bauch & Michaels</li>
+                  <li>Belin, Lamson, McCormick, Zumbach, Flynn</li>
+                  <li>Berstein, Stair & McAdams LLP</li>
+                  <li>Blackwell, Sanders, Peper, Martin LLP</li>
+                  <li>Boone, Smith, Davis, Hurst & Dickman</li>
+                  <li>Bryan Cave LLP</li>
+                  <li>Buchanan & Ingersoll</li>
+                  <li>Butzel Long</li>
+                  <li>Cavitch, Familo, Durkin & Frutkin</li>
+                  <li>Chapman & Cutler</li>
+                  <li>Cohen, Grossman & Rosenson LLP</li>
+                  <li>Cooper, Erving & Savage LLP</li>
+                  <li>Curtis, Mallet, Prevost, Colt & Mosle</li>
+                  <li>D'Ancona & Pflaum LLP</li>
+                  <li>D., McCarthy, Taylor, Finley, Bryant & Lee LLP</li>
+                  <li>Duane, Morris, Heckscher LLP</li>
+                  <li>Faegre & Benson LLP</li>
+                  <li>Foley & Lardner</li>
+                  <li>Forman, Perry, Watkins, Krutz & Tardy</li>
+                  <li>Frost, Brown, Todd LLC</li>
+                  <li>Godfrey, Neshek, Worth & Leibsle</li>
+                  <li>G.K.B.Black, Rosenbloom & Moritz, Ltd.</li>
+                  <li>Gould & Ratner</li>
+                  <li>Greenberg Traurig LLP</li>
+                  <li>Hahn & Hessen LLP</li>
+                  <li>Holcomb & Fletcher PLLC</li>
+                  <li>Jenner & Block</li>
+                  <li>Jessop & Company</li>
+                  <li>Jones, Day</li>
+                </ul>
+              </div>
+
+              <div className="col-12 col-md-4">
+                <ul>
+                  <li>Katten Muchin Zavis Rosenman</li>
+                  <li>Katz, Randall, Weinberg & Richmond</li>
+                  <li>Kaye Scholer LLP</li>
+                  <li>Kelly, Drye & Warren LLP</li>
+                  <li>Kennedy Covington</li>
+                  <li>King & Spalding LLP</li>
+                  <li>Kirkland & Ellis</li>
+                  <li>Kluger, Peretz, Kaplan & Berlin PL</li>
+                  <li>Lamberth, Cifelli, Stokes & Stout PA</li>
+                  <li>Latham & Watkins LLP</li>
+                  <li>Lazar, Levine & Felix LLP</li>
+                  <li>Levin & Funkhouser Ltd.</li>
+                  <li>Lewis, Rice & Fingerish</li>
+                  <li>Lindquist & Vennum PLLP</li>
+                  <li>Locke, Liddle & Sapp LLP</li>
+                  <li>Lord, Bissel & Brook</li>
+                  <li>MacKall, Crounse & Moore PLC</li>
+                  <li>Mayer, Brown, Rowe & Maw</li>
+                  <li>McDermott, Will & Emery</li>
+                  <li>McKenna, Long, Aldridge LLP</li>
+                  <li>McLain & Merritt</li>
+                  <li>Michael, Best & Friedrick LLP</li>
+                  <li>Miles & Stockbridge PC</li>
+                  <li>Morrison & Hecker LLP</li>
+                  <li>M.S.Freed Denenberg Ament & Rubenstein, PC</li>
+                  <li>Murphy Desmond SC</li>
+                  <li>Neal, Gerber & Eisenberg</li>
+                  <li>Neal & Harwell PLC</li>
+                  <li>Ober, Kaler, Grimes & Shriver</li>
+                  <li>O.C.H.S. Steindler, Houston & Rosen PC</li>
+                  <li>Parker, Hudson, Rainer & Dobbs LLP</li>
+                  <li>Parsons, Davies, Kinghorn & Peters</li>
+                  <li>Paul, Hastings, Janofsky & Walker LLP</li>
+                  <li>Piper Rudnick LLP</li>
+                  <li>Porter, Wright, Morris & Arthur LLP</li>
+                </ul>
+              </div>
+
+              <div className="col-12 col-md-4">
+                <ul>
+                  <li>Quirk & Bakalor PC</li>
+                  <li>Reinhart, Boerner, VanDeuren SC</li>
+                  <li>Resnick, Fedder & Silverman</li>
+                  <li>Richards, Layton & Finger</li>
+                  <li>Sachnoff Weaver Ltd.</li>
+                  <li>Schiff, Hardin & Waite</li>
+                  <li>S. F. N. & Rosenberg Ltd.</li>
+                  <li>S. C. G. K. Seiller & Handmaker LLP</li>
+                  <li>S. G. F. Glantz Wolfson & Towbin LLC</li>
+                  <li>Shefsky & Froelich Ltd.</li>
+                  <li>Sidley Austin Brown & Wood LLP</li>
+                  <li>Skadden, Arps, Slate, Meagher & Flom LLP</li>
+                  <li>Sommer Barnard Ackerson PC</li>
+                  <li>Sonnenschein, Carlin, Nath & Rosenthal</li>
+                  <li>Spector, Cohen, Gadon, Rosen</li>
+                  <li>Stutman Triester & Glatt</li>
+                  <li>Summers, Compton, Wells & Hamburg</li>
+                  <li>Thompson Hine</li>
+                  <li>Troutman Sanders LLP</li>
+                  <li>Vedder Price Kaufman & Kammholz P.C.</li>
+                  <li>Vorys, Sater, Seymour & Pease LLP</li>
+                  <li>Warner, Norcross & Judd LLP</li>
+                  <li>Westervelt, Johnson, Nicoll & Keller</li>
+                  <li>Whyte Hirschboeck Dudek SC</li>
+                  <li>Winston & Strawn</li>
+                  <li>Wolf, Bloc, Schorr & Solis-Cohen</li>
+                  <li>Wyatt, Tarrant & Combs LLP</li>
+                  <li>Young, Conaway, Stargatt & Taylor LLP</li>
+                </ul>
+              </div>
+
+            </div>
+
+            <div className="row mt-4 ml-4 mr-4">
+              <div className="col-12 mb-3">
+                <h5>Lenders/Banks/Financial Institutions</h5>
+              </div>
+              <div className="col-12 col-md-4  mb-5">
+                <ul>
+                  <li>ABN AMRO</li>
+                  <li>Access Capital</li>
+                  <li>Albion Alliance</li>
+                  <li>Allstate Financial Corporation</li>
+                  <li>AmSouth Bank</li>
+                  <li>American Security Bank</li>
+                  <li>Ameritrust Company</li>
+                  <li>Associated Bank</li>
+                  <li>Associates Commercial</li>
+                  <li>Back Bay Capital</li>
+                  <li>Banco Popular</li>
+                  <li>Bank Austria</li>
+                  <li>Bank Boston</li>
+                  <li>Bank of America</li>
+                  <li>Bank of New York</li>
+                  <li>Bank One</li>
+                  <li>BMO Nesbitt Burns</li>
+                  <li>Brickyard Bank</li>
+                  <li>C&S Sovran</li>
+                  <li>Cargill Financial Services Corp.</li>
+                  <li>Cerberus Capital</li>
+                  <li>CIB Bank</li>
+                  <li>CIT Business Credit</li>
+                  <li>Citicapital Commercial Bank</li>
+                  <li>Citicorp</li>
+                  <li>City Bank</li>
+                  <li>CoBank</li>
+                  <li>Cole Taylor</li>
+                  <li>Comerica</li>
+                  <li>Congress Financial</li>
+                  <li>Deutsche Bank</li>
+                  <li>Equitable</li>
+                  <li>Fidelity National Bank</li>
+                  <li>Fifth Third</li>
+                  <li>FINOVA</li>
+                  <li>First American</li>
+                  <li>First American National</li>
+                </ul>
+              </div>
+
+              <div className="col-12 col-md-4">
+                <ul>
+                  <li>First Banking Center</li>
+                  <li>First Business Bank</li>
+                  <li>First Midwest Bank</li>
+                  <li>First Interstate Bank of California</li>
+                  <li>First Business Capital Corp.</li>
+                  <li>First Source Bank</li>
+                  <li>First Union Business Credit</li>
+                  <li>First Union Capital Markets Group</li>
+                  <li>First Union National Bank</li>
+                  <li>Fleet Bank</li>
+                  <li>Fleet Capital Corporation</li>
+                  <li>FNCB</li>
+                  <li>Frost Capital Group</li>
+                  <li>GE Capital Business Credit</li>
+                  <li>GE Capital Commercial Finance, Inc.</li>
+                  <li>GECC</li>
+                  <li>Glenview State Bank</li>
+                  <li>GMAC Business Credit</li>
+                  <li>Harris Bank</li>
+                  <li>Hong Kong Bank</li>
+                  <li>Household Finance</li>
+                  <li>HSBC</li>
+                  <li>Huntington Bank</li>
+                  <li>IBJ Schroder</li>
+                  <li>ING</li>
+                  <li>John Hancock</li>
+                  <li>JP Morgan Chase</li>
+                  <li>Key Bank</li>
+                  <li>LakeView Bank</li>
+                  <li>LaSalle Bank</li>
+                  <li>Lyon Credit Corporation</li>
+                  <li>Manufacturers Bank</li>
+                  <li>Marine Bank</li>
+                  <li>Marine Midland Business Loans</li>
+                  <li>Marshall and Isley</li>
+                  <li>Maryland National Credit Corporation</li>
+                </ul>
+              </div>
+
+              <div className="col-12 col-md-4">
+                <ul>
+                  <li>Mellon Bank</li>
+                  <li>Merrill Lynch Business Credit</li>
+                  <li>Milwaukee Western Bank</li>
+                  <li>MNC Commercial Corporation</li>
+                  <li>National Bank of Canada</li>
+                  <li>National Boulevard Bank</li>
+                  <li>National Canada Finance</li>
+                  <li>National City Bank</li>
+                  <li>NCNB National Bank</li>
+                  <li>Northern Trust</li>
+                  <li>PNC Bank</li>
+                  <li>PNC Business Credit</li>
+                  <li>PPM America</li>
+                  <li>Rabobank</li>
+                  <li>Royal Bank of Canada</li>
+                  <li>Roynat, Inc.</li>
+                  <li>Signet Bank</li>
+                  <li>Society National Bank</li>
+                  <li>South Trust Bank</li>
+                  <li>Standard Federal Bank</li>
+                  <li>Star Bank</li>
+                  <li>State Street Bank</li>
+                  <li>Sterling Bank</li>
+                  <li>Suburban Bank</li>
+                  <li>Sun Bank</li>
+                  <li>SunTrust Bank</li>
+                  <li>Texas Capital Bank</li>
+                  <li>Transamerica</li>
+                  <li>Union Bank</li>
+                  <li>UPS Capital</li>
+                  <li>US Bank</li>
+                  <li>Valley National Bank</li>
+                  <li>Wachovia</li>
+                  <li>Wells Fargo</li>
+                  <li>Wells Fargo Foothill</li>
+                  <li>West Bank</li>
+                  <li>Zion Bank</li>
+                </ul>
+              </div>
             </div>
 
           </div>
