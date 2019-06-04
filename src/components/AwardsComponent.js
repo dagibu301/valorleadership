cb04bc6f79decf6e917edc217b5a05d63868c634
IT dept Valor Homes <itdept@valorhomes100llc.com>
2019-06-04 11:30:41 -0400

    awards

--- src/components/AwardsComponent.js
+++ src/components/AwardsComponent.js
@@ -15,7 +15,7 @@ function Awards () {
                       <source media="(max-width: 1199px)" srcset="/assets/images/banner2.jpg" />
                       <img src="/assets/images/banner2.jpg"  />
                     </picture>
-                    <h2 className="tittles-text">Awards</h2>
+                    <h2 className="tittles-text">Awards  and Publications</h2>
                 </div>
               </div>
             </div>
@@ -25,7 +25,83 @@ function Awards () {
 
         <div className="container homebody tabs">
           <div className="container innerhomebody">
-            <h2>Awards</h2>
+
+          <div className="row mt-4 ml-4 mr-4">
+            <div className="col-12">
+              <h2>Awards</h2>
+            </div>
+          </div>
+
+          <div className="row">
+            <div className="col-12 offset-md-1 col-md-10 mt-3">
+              <table class="table  table-bordered table-hover">
+                <thead class="thead-dark1">
+                  <tr>
+                    <th scope="col">Award Title</th>
+                    <th scope="col">Prsented By</th>
+                  </tr>
+                </thead>
+                <tbody>
+                  <tr>
+                    <td>Proin elementum feugiat risus foin consequat ultricies mi</td>
+                    <td>Vitae lacus at diam feugiat</td>
+                  </tr>
+                  <tr>
+                    <td>Etiam eu erat sagittis magna ornare suscipit. Sed sollicitudin neque quis sem</td>
+                    <td>Lacus at diam feugiat</td>
+                  </tr>
+                  <tr>
+                    <td>Cras id arcu nec risus posuere tincidunt. Praesent varius congue odio.</td>
+                    <td>Atdiam feugiat</td>
+                  </tr>
+
+                  <tr>
+                    <td>Donec arcu odio, porta vitae, commodo vitae, molestie vel, est.</td>
+                    <td>Giam feugiat</td>
+                  </tr>
+                </tbody>
+              </table>
+            </div>
+          </div>
+
+          <div className="row mt-4 ml-4 mr-4">
+            <div className="col-12">
+              <h2>PUBLICATIONS</h2>
+            </div>
+          </div>
+
+          <div className="row">
+            <div className="col-12 offset-md-1 col-md-10 mt-3 mb-5">
+              <table class="table  table-bordered table-hover">
+                <thead class="thead-dark1">
+                  <tr>
+                    <th scope="col">Title</th>
+                    <th scope="col">Publicatins</th>
+                  </tr>
+                </thead>
+                <tbody>
+                  <tr>
+                    <td>Proin elementum feugiat risus foin consequat ultricies mi</td>
+                    <td>Vitae lacus at diam feugiat</td>
+                  </tr>
+                  <tr>
+                    <td>Etiam eu erat sagittis magna ornare suscipit. Sed sollicitudin neque quis sem</td>
+                    <td>Lacus at diam feugiat</td>
+                  </tr>
+                  <tr>
+                    <td>Cras id arcu nec risus posuere tincidunt. Praesent varius congue odio.</td>
+                    <td>Atdiam feugiat</td>
+                  </tr>
+
+                  <tr>
+                    <td>Donec arcu odio, porta vitae, commodo vitae, molestie vel, est.</td>
+                    <td>Giam feugiat</td>
+                  </tr>
+                </tbody>
+              </table>
+            </div>
+          </div>
+
 
           </div>
         </div>
